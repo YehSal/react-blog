4ab0aad82b1069f0bb041a1b655b6ec3fbf1bee7
Yehia Saleh <yehia.saleh@yale.edu>
2017-06-02 16:42:44 -0400

    Setup the application, started building new post form for posts API

---
+++ src/components/posts_new.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <lable>{field.label}</lable>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
+
+  renderTagsField(field) {
+    return (
+      <div className="form-group">
+        <lable>Tags</lable>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+         />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({form: 'PostsNewForm'})(PostsNew);
